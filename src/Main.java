4c65dc8509dbf5b9a92958200061739f49c1150e
1999Snehal <snehallagad84@gmail.com>
2024-11-05 15:23:02 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import  java.util.*;
+ class  Account
+ {
+     public  String name;
+     protected String email;
+     private  String password;
+
+     public  String getPassword()
+     {
+         return  this.password;
+     }
+
+     public void  setPassword(String pass)
+     {
+         this.password = pass;
+     }
+ }
+
+  class Encapsulation
+ {
+    public  static  void  main(String args[])
+    {
+        Account account1= new Account();
+        account1.name = "Sonam kapur";
+        account1.email = "sonamkapur@gmail.com";
+        account1.setPassword("abcd");
+        System.out.println(account1.getPassword());
     }
-}
+ }
